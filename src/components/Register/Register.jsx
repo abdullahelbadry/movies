e945c35bf9b9dd9a846d1cad687186912f2b74ed
Abdullah Elbadry <45333450+abdullahelbadry@users.noreply.github.com>
2022-08-29 14:59:36 +0200

    register page completely done

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import Joi from 'joi';
 
 export default function Register() {
 
-
+  const [errorList, setErrorList] = useState([])
   const [error, setError] = useState('')
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState({
@@ -14,16 +15,36 @@ export default function Register() {
     password: ''
   });
 
-  
-  function getUser(e){
-    let myUser = {...user}
+  function getUser(e) {
+    let myUser = { ...user }
     myUser[e.target.name] = e.target.value;
     setUser(myUser);
   }
 
-  async function submitRegister(e){
+  async function submitRegister(e) {
     e.preventDefault();
     setIsLoading(true);
+      setError();
+    let validateResult = validateRegisterForm(user)
+
+    if (validateResult.error) {
+      setIsLoading(false);
+      setErrorList(validateResult.error.details);
+
+    } else {
+      
+      let { data } = await axios.post(`https://routeegypt.herokuapp.com/signup`, user);
+      if (data.message === "success") {
+        setIsLoading(false);
+        console.log("Successfully Registered" + data.message);
+      } else {
+        setIsLoading(false);
+        setError(data.message)
+      }
+    }
+
+
+    /* setIsLoading(true);
     setError();
     let {data} = await axios.post(`https://routeegypt.herokuapp.com/signup`, user);
     if(data.message=== "success"){
@@ -32,21 +53,43 @@ export default function Register() {
     } else{
       setIsLoading(false);
       setError(data.message)
-    }
+    } */
   }
+
+  /* Start Of Validation Function */
+  function validateRegisterForm(user) {
+    var schema = Joi.object({
+      first_name: Joi.string().min(3).max(20).required(),
+      last_name: Joi.string().min(3).max(20).required(),
+      age: Joi.number().min(16).max(60).required(),
+      email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+      password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    });
+    return schema.validate(user, {abortEarly:false})
+  }
+  /* Start Of Validation Function */
 
   return (
     <div>
 
       <h2 className='my-3 text-center'>Register Now</h2>
 
-      {error?<div className="alert alert-danger">{error}</div> : ''}
+      {errorList.map((error, index)=>{
+        if(index===4){
+          return <div key={index} className='alert alert-danger '>Invalid Password</div>
+        } else {
+          return <div key={index} className='alert alert-danger '>
+          {error.message}</div>
+        }
+})}
+
+      {error ? <div className="alert alert-danger">{error}</div> : ''}
 
       <form onSubmit={submitRegister}>
         <label htmlFor='first_name' className='mt-3'>First Name:</label>
         <input onChange={getUser} type="text" className="form-control" name='first_name' id='first_name' />
 
-        <label htmlFor='last_name'  className='mt-3'>Last Name:</label>
+        <label htmlFor='last_name' className='mt-3'>Last Name:</label>
         <input onChange={getUser} type="text" className="form-control" name='last_name' id='last_name' />
 
         <label htmlFor='age' className='mt-3'>Age: </label>
@@ -59,7 +102,7 @@ export default function Register() {
         <input onChange={getUser} type="password" className="form-control" name='password' id='password' />
 
         <button className='btn btn-outline-info mt-3'>
-          {isLoading? <i className="fas fa-spinner fa-spin"></i>: 'Register'}
+          {isLoading ? <i className="fas fa-spinner fa-spin"></i> : 'Register'}
         </button>
       </form>
     </div>
