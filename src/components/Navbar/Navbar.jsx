2b0b090b6860075d33ce34b2105f71a8d2038029
Abdullah Elbadry <45333450+abdullahelbadry@users.noreply.github.com>
2022-08-29 08:37:29 +0200

    social media added and navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,8 +31,13 @@ export default function Navbar() {
               <Link className="nav-link" to={'about'}>About</Link>
             </li>
           </ul>
-
           <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+            <li className='nav-item me-3 d-flex align-items-center'>
+            <i class="fa-brands mx-2 fa-facebook"></i>
+            <i class="fa-brands mx-2 fa-youtube"></i>
+            <i class="fa-brands mx-2 fa-instagram"></i>
+            <i class="fa-brands mx-2 fa-spotify"></i>
+            </li>
             <li className="nav-item">
               <Link className="nav-link" to={'register'}>Register</Link>
             </li>
